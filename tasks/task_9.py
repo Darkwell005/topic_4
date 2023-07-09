be509721b3aa2dcc4d878ffc058893c8db2c4309
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-07-09 11:27:20 +0100

    Домашнее задание | 4. Основные операторы
    - Разбор задач: 1, 9

--- tasks/task_9.py
+++ tasks/task_9.py
@@ -1,4 +1,5 @@
 num_switches: int = int(input('Введите количество переключателей: '))
 
-combinations = 2 ** num_switches
-print('Количество возможных комбинаций:',combinations)
+combinations: int = 2 ** num_switches
+
+print('Количество возможных комбинаций:', combinations)
